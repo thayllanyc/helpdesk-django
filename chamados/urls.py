f3f70e94789ad639bbbe7d04e67256ab16f749d0
Thayllany Correa <thayllanycorrea653@gmail.com>
2026-08-16 19:51:38 -0300

    Aplica Bootstrap e estiliza tela de login

--- chamados/urls.py
+++ chamados/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="chamados/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("", views.lista_chamados, name="lista_chamados"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("novo/", views.criar_chamado, name="criar_chamado"),
